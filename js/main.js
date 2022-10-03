a0a48749a1e55602b8766b57e186ecea31f10b64
AytacMensimova22 <aytacmensimova22@gmail.com>
2022-10-04 03:40:20 +0400

    js-1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+var name = "Aytac"
+var surname = "Mənsimova-dır"
+var date = "11"
+var month = "12"
+var year = "1990"
+var color = "mavidir"
+var hobbi = "kitab oxumaq və musiqiyə qulaq asmaqdır"
+var current year = 2022
+var born = 1990
+var work = "proqramçı"
+
+
+
+
+document.getElementsByClassName("demo")[0].innerHTML = ("Mənim adım"+" "+name+","+"soyadım isə"+" "+surname+"."+"Mən"+" "+date+"."+month+"."+year+" "+"tarixində dünyaya gəlmişəm."+
+"Mənim sevdiyim rəng"+" "+color+"."+"Mənim hobbim"+" "+hobbi+"."+"Hal hazırda"+" "+(current year-born)+" "+"yaşım var."+"Mən"+" "+work+" "+"olacam.")
